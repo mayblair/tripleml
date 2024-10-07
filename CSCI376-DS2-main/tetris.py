19b667e6d444eb286cac553cf8afac8a04572865
Lola Casenave <lgc3@williams.edu>
2024-10-07 15:37:56 -0400

    added pause 0.25

--- CSCI376-DS2-main/tetris.py
+++ CSCI376-DS2-main/tetris.py
@@ -82,6 +82,7 @@ def main():
                 # Example of pressing keys with pyautogui based on other recognized gestures
                 if recognized_gesture == "Open_Palm":
                     pyautogui.press("up")
+                    pyautogui.PAUSE = 0.25 
                 elif recognized_gesture == "Thumb_Down":
                     pyautogui.press("down")
                 elif recognized_gesture == "Thumb_Up":
